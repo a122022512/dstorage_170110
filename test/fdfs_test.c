5c23d637e4991ca1cf3fa11bf06a33311b56f5ba
a122022512 <ws_bing@126.com>
2017-05-19 15:30:56 +0800

    天

---
+++ test/fdfs_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>                                                                                                                                                         
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>   
+#include <sys/wait.h>
+
+int fdfs_log(const char *filename,char *file)
+{
+    
+}
+
